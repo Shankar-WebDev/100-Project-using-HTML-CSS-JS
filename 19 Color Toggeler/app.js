e1788dcbbb6d0ddadd472b0cd9afa424f328c625
Sankaranarayanan <sankar241101@gmail.com>
2024-11-06 12:10:29 +0530

    Project -19

---
+++ 19 Color Toggeler/app.js
@@ -0,0 +1,15 @@
+let toggler = document.getElementById('switch');
+
+toggler.addEventListener('click', () => {
+//   if (toggler.checked === true) {
+//     document.body.style.backgroundColor = '#171717';
+//   } else {
+//     document.body.style.backgroundColor = '#F2F2F7';
+//   }
+
+
+    toggler.checked === true
+    
+    ?(document.body.style.backgroundColor = "#2C2C2E")
+    :(document.body.style.backgroundColor = "#F7F7F7")
+});
