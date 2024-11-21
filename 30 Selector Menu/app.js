c57d5797d655a2111e52aeea4b6a2c7ad382284a
Sankaranarayanan <sankar241101@gmail.com>
2024-11-21 19:06:36 +0530

    Project 30

---
+++ 30 Selector Menu/app.js
@@ -0,0 +1,27 @@
+//selectField
+
+let selectField = document.getElementById('selectField');
+
+//selectText
+
+let selectText = document.getElementById('selectText');
+
+let list = document.getElementById('list');
+
+let arrowIcon = document.getElementById('arrowIcon');
+
+let options = document.getElementsByClassName('options');
+
+selectField.addEventListener('click', () => {
+  list.classList.toggle('hide');
+  arrowIcon.classList.toggle('rotate')
+});
+
+
+for(option of options){
+    option.onclick = function(){
+        selectText.innerHTML = this.textContent;
+        list.classList.toggle('hide');
+        arrowIcon.classList.toggle('rotate');
+    }
+}
