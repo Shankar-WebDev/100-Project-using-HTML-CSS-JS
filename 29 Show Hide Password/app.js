fd2b709b78e385f7f052b534c599f378e1dfa8b2
Sankaranarayanan <sankar241101@gmail.com>
2024-11-21 18:15:33 +0530

    Project 29

---
+++ 29 Show Hide Password/app.js
@@ -0,0 +1,16 @@
+const input = document.querySelector('input');
+
+// eye
+
+const eye = document.querySelector('#eye-icon');
+
+eye.addEventListener('click', () => {
+  // if(input.type=== 'password'){
+  //     input.type==='text';
+  // }else{
+  //     input.type==='password';
+  // }
+
+  //Refactor Code
+  input.type === 'password' ? (input.type = 'text') : (input.type = 'password');
+});
